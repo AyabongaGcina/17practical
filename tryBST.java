b9380fbda98b5950b9898b10388f05915cd64a21
AyabongaGcina <4446494@myuwc.ac.za>
2026-03-25 12:26:08 +0000

    Inserting the method

--- tryBST.java
+++ tryBST.java
@@ -1,2 +1,13 @@
 import java.util.*;
 //Main Binary search tree class
+public class tryBST {
+    private tNode root;
+    private int nodeCount;
+    //----Constructor----
+    public tryBST() {
+        root = null;
+        nodeCount = 0;
+    }
+    //Inserting the method
+    
+}
