87511a719c88bbfb5ec69bcef5817974b02fde74
AyabongaGcina <4446494@myuwc.ac.za>
2026-03-25 12:20:58 +0000

    imported java packages

---
+++ tNode.java
@@ -0,0 +1,11 @@
+public class tNode{
+    int key;
+    tNode left;  //left child
+    tNode right; //right child
+    //Constructor
+    public tNode(int key){
+        this.key=key;
+        this.left=null;
+        this.right=null;
+    }
+}
