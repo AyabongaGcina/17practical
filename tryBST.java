f1eabf3ab884c510f4c6e3de1ed652766b9cf2e9
AyabongaGcina <4446494@myuwc.ac.za>
2026-03-25 12:29:53 +0000

    Instruct itn if key=node.key,do nothing

--- tryBST.java
+++ tryBST.java
@@ -9,5 +9,22 @@ public class tryBST {
         nodeCount = 0;
     }
     //Inserting the method
-    
+    public void insert(int key) {
+        root = insertRec(root, key);
+        nodeCount++;
+    }
+    private tNode insertRec(tNode node, int key) {
+        if (node == null) {
+            return new tNode(key);
+        }
+         if (key < node.key) {
+            node.left = insertRec(node.left, key);
+        } else if (key > node.key) {
+            node.right = insertRec(node.right, key);
+        }
+        // If key equals node.key, do nothing (no duplicates)
+        
+        return node;
+    }
+
 }
